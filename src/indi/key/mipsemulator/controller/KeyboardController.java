09090a99f814ccfdec319784c1dcb84ff0209d37
Chen Mouxiang <cmx_1007@foxmail.com>
2019-05-01 12:16:43 +0800

    [REFACT] ps2 memory.

--- src/indi/key/mipsemulator/controller/KeyboardController.java
+++ src/indi/key/mipsemulator/controller/KeyboardController.java
@@ -7,21 +7,18 @@ import indi.key.mipsemulator.core.controller.Machine;
 import indi.key.mipsemulator.model.exception.MemoryOutOfBoundsException;
 import indi.key.mipsemulator.model.info.BitArray;
 import indi.key.mipsemulator.model.info.PS2Key;
-import indi.key.mipsemulator.model.interfaces.TickCallback;
 import indi.key.mipsemulator.storage.ByteArrayMemory;
-import indi.key.mipsemulator.storage.Memory;
 import indi.key.mipsemulator.storage.MemoryType;
 import indi.key.mipsemulator.util.LogUtils;
 import javafx.scene.input.KeyCode;
 
-public class KeyboardController implements TickCallback {
+public class KeyboardController {
 
-    private Queue<Byte> keyQueue = new LinkedBlockingQueue<>();
-    private Memory ps2Memory;
+
+    private PS2Memory ps2Memory;
 
     public KeyboardController(Machine machine) {
-        machine.addCpuListener(this);
-        ps2Memory = machine.getAddressRedirector().getMemory(MemoryType.PS2);
+        ps2Memory = (PS2Memory) machine.getAddressRedirector().getMemory(MemoryType.PS2);
     }
 
     public void press(KeyCode keyCode) {
@@ -29,7 +26,7 @@ public class KeyboardController implements TickCallback {
         LogUtils.i(ps2Key);
         byte[] pressCode = ps2Key.getPressCode();
         for (byte b : pressCode) {
-            keyQueue.add(b);
+            ps2Memory.push(b);
         }
     }
 
@@ -38,25 +35,17 @@ public class KeyboardController implements TickCallback {
         LogUtils.i(ps2Key);
         byte[] releaseCode = ps2Key.getReleaseCode();
         for (byte b : releaseCode) {
-            keyQueue.add(b);
-        }
-    }
-
-    @Override
-    public void onTick() {
-        byte[] bytes = ps2Memory.load(0, 4);
-        if ((bytes[3] & 0x80) == 0) {
-            Byte key = keyQueue.poll();
-            if (key != null) {
-                bytes[3] = 0b1000;
-                bytes[0] = key;
-                LogUtils.i(Integer.toHexString(Byte.toUnsignedInt(key)));
-                ps2Memory.save(0, bytes);
-            }
+            ps2Memory.push(b);
         }
     }
 
     public static class PS2Memory extends ByteArrayMemory {
+
+        private Queue<Byte> keyQueue = new LinkedBlockingQueue<>();
+
+        void push(Byte b) {
+            keyQueue.add(b);
+        }
 
         public PS2Memory(int depth) {
             super(depth);
@@ -64,9 +53,17 @@ public class KeyboardController implements TickCallback {
 
         @Override
         public byte[] load(long address, int bytesNum) throws MemoryOutOfBoundsException {
-            byte[] bytes = super.load(address, bytesNum);
-            save(address, BitArray.of(bytes).set(31, false).bytes());
-            return bytes;
+            Byte key = keyQueue.poll();
+            byte[] bytes = super.load(0, 4);
+            if (key != null) {
+                bytes[3] = 0b1000;
+                bytes[0] = key;
+                LogUtils.i(Integer.toHexString(Byte.toUnsignedInt(key)));
+            } else {
+                bytes = BitArray.of(bytes).set(31, false).bytes();
+            }
+            super.save(0, bytes);
+            return super.load(address, bytesNum);
         }
     }
 }
