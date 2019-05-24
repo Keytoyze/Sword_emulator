03ceee53d616974740af07619452a63ca2011669
ChenMouxiang <cmx_1007@foxmail.com>
2019-05-24 19:44:07 +0800

    [UPDATE] multi-cycle processors ui.

---
+++ src/indi/key/mipsemulator/controller/stage/LoadFileController.java
@@ -0,0 +1,25 @@
+package indi.key.mipsemulator.controller.stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoadFileController implements Initializable {
+
+    public TextField singleRam;
+    public Button singleRamChoose;
+    public TextField singleRom;
+    public Button singleRomChoose;
+    public Button singleLoad;
+    public TextField multiRam;
+    public Button multiRamChoose;
+    public Button multiLoad;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
