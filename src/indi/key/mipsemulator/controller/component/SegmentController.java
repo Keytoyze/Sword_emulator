275075ad5bdf8aca36985266f213d22e72f6f95c
ChenMouxiang <cmx_1007@foxmail.com>
2019-05-28 13:31:54 +0800

    [UPDATE] readme.

--- src/indi/key/mipsemulator/controller/component/SegmentController.java
+++ src/indi/key/mipsemulator/controller/component/SegmentController.java
@@ -14,6 +14,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
+// FIXME: In some case the canvas will be killed and cannot draw anything. It seems to result from JavaFX internal widgets.
 public class SegmentController implements TickCallback {
 
     private Machine machine;
