80e741dbea6b2790a972bff72e2d4830e65cf98e
ChenMouxiang <cmx_1007@foxmail.com>
2019-06-11 12:36:29 +0800

    [FIX] include jfxrt.jar to adapt jdk 11.

--- src/indi/key/mipsemulator/Main.java
+++ src/indi/key/mipsemulator/Main.java
@@ -7,11 +7,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         SwordController.run(primaryStage);
     }
 
     public static void main(String[] args) {
+        // UPDATE in v1.0.1: See https://stackoverflow.com/questions/31786980/javafx-combobox-not-responding-on-windows-10
+        System.setProperty("glass.accessible.force", "false");
         launch(args);
     }
 }
